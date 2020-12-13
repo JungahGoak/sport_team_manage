e05bc8f51e200d9d618e711b109283f787771db7
Jungahgo <70677614+Jungahgo@users.noreply.github.com>
2020-12-13 23:48:10 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,5 @@
 #include "header.h"
 
-//구단 선수
-
-struct node {
-    char position[20];
-    char name[20];
-    int gameScore;
-    int comScore;
-    int potenScore;
-    struct node* next;
-};
-
 FILE* fp = NULL;
 int perGame;
 int perCom;
