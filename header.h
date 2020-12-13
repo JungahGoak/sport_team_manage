78cd7220038feed92e911e4a06be7fa8e59593e2
Jungahgo <70677614+Jungahgo@users.noreply.github.com>
2020-12-13 23:48:24 +0900

    Update header.h

--- header.h
+++ header.h
@@ -1,3 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+//구단 선수
+
+struct node {
+    char position[20];
+    char name[20];
+    int gameScore;
+    int comScore;
+    int potenScore;
+    struct node* next;
+};
